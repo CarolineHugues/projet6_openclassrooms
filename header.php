b1dfda3440fc337b9994f81bd2f5407e51c3bddc
Caroline Hugues <contact@hc-projet1.ovh>
2018-10-08 21:41:49 +0200

    Elements of header of parent template no used deleted

--- header.php
+++ header.php
@@ -18,6 +18,8 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"> 
+ 
 
 <?php wp_head(); ?>
 </head>
@@ -27,8 +29,6 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentyseventeen' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-
-		<?php get_template_part( 'template-parts/header/header', 'image' ); ?>
 
 		<?php if ( has_nav_menu( 'top' ) ) : ?>
 			<div class="navigation-top">
