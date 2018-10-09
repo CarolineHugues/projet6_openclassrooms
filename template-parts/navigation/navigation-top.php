14a88992effefb3fd6f784b24b209c4edf4dca0f
Caroline Hugues <contact@hc-projet1.ovh>
2018-10-09 20:43:08 +0200

    Logo - mobile menu and social menu added

--- template-parts/navigation/navigation-top.php
+++ template-parts/navigation/navigation-top.php
@@ -10,17 +10,35 @@
 
 ?>
 <nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Top Menu', 'twentyseventeen' ); ?>">
-	<button class="menu-toggle" aria-controls="top-menu" aria-expanded="false">
-		<?php
-		echo twentyseventeen_get_svg( array( 'icon' => 'bars' ) );
-		echo twentyseventeen_get_svg( array( 'icon' => 'close' ) );
-		_e( 'Menu', 'twentyseventeen' );
-		?>
-	</button>
+	<section id="nav-top">
+		<div id="logo-bde">
+			<?php the_custom_logo(); ?>
+		</div>
 
+		<button class="menu-toggle" aria-controls="top-menu" aria-expanded="false">
+			<?php
+			echo twentyseventeen_get_svg( array( 'icon' => 'bars' ) );
+			echo twentyseventeen_get_svg( array( 'icon' => 'close' ) );
+			?>
+		</button>
+	
+		<?php wp_nav_menu( array(
+			'theme_location' => 'top',
+			'menu_id'        => 'top-menu',
+		) ); ?>
+
+		<?php if ( has_nav_menu( 'social' ) ) : ?>
+			<?php wp_nav_menu( array(
+				'theme_location' => 'social',
+				'menu_id'     => 'top-social-menu',
+				) );
+			?>
+		<?php endif; ?>
+	</section>
+	
 	<?php wp_nav_menu( array(
-		'theme_location' => 'top',
-		'menu_id'        => 'top-menu',
+		'theme_location' => 'top-mobile-menu',
+		'menu_id'        => 'top-mobile-menu',
 	) ); ?>
 
 </nav><!-- #site-navigation -->
