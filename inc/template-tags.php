24a6cdc94c10eb7576b2b1e508b9da7ef182bed4
Caroline Hugues <contact@hc-projet1.ovh>
2018-10-14 22:02:15 +0200

    Customize home panels

---
+++ inc/template-tags.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Custom template tags for this theme
+ *
+ */
+
+/**
+ * Display a front page section.
+ *
+ * @param WP_Customize_Partial $partial Partial associated with a selective refresh request.
+ * @param integer              $id Front page section to display.
+ */
+function twentyseventeen_child_front_page_section( $partial = null, $id = 0 ) {
+	if ( is_a( $partial, 'WP_Customize_Partial' ) ) {
+		// Find out the id and set it up during a selective refresh.
+		global $twentyseventeencounter;
+		$id = str_replace( 'panel_', '', $partial->id );
+		$twentyseventeencounter = $id;
+	}
+
+	global $post; // Modify the global post object before setting up post data.
+	if ( get_theme_mod( 'panel_' . $id ) ) {
+		$post = get_post( get_theme_mod( 'panel_' . $id ) );
+		setup_postdata( $post );
+		set_query_var( 'panel', $id );
+
+		$post_panel_id = get_the_ID();
+		if ($post_panel_id == 109)
+		{
+			get_template_part( 'template-parts/page/content', 'section-upload-documents' );
+		}
+		else
+		{
+			get_template_part( 'template-parts/page/content', 'front-page-panels' );
+		}
+
+		wp_reset_postdata();
+	} elseif ( is_customize_preview() ) {
+		// The output placeholder anchor.
+		echo '<article class="panel-placeholder panel twentyseventeen-panel twentyseventeen-panel' . $id . '" id="panel' . $id . '"><span class="twentyseventeen-panel-title">' . sprintf( __( 'Front Page Section %1$s Placeholder', 'twentyseventeen' ), $id ) . '</span></article>';
+	}
+}
