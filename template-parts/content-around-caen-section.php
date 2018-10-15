70d59fd20280990c58cdecb0128b99d32f424428
Caroline Hugues <contact@hc-projet1.ovh>
2018-10-15 19:53:14 +0200

    template around caen section created

---
+++ template-parts/content-around-caen-section.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template part for displaying around caen section on Caen page
+ *
+ */
+
+$id = 131;
+$included_page = get_post( $id );
+$title = apply_filters('the_title', $included_page->post_title);
+$content = apply_filters('the_content', $included_page->post_content); 
+
+?>
+
+<article id="around-caen-section">
+	
+    <div class="wrap">
+    	<header class="entry-header">
+    		<h2 class="entry-title"><?php echo $title; ?></h2>
+    		<?php twentyseventeen_edit_link( get_the_ID() ); ?>
+    	</header><!-- .entry-header -->
+
+    	<div class="entry-content">
+        	<?php echo $content; ?>
+    	</div><!-- .entry-content -->	
+	</div><!-- .wrap -->
+	
+</article><!-- #post-## -->
