45219deae8acf3f302649289c29e1c771b1783b8
Caroline Hugues <contact@hc-projet1.ovh>
2018-10-16 01:49:10 +0200

    widgets area added to front page template

--- front-page.php
+++ front-page.php
@@ -1,32 +1,56 @@
 <?php
 /**
- * Twenty Seventeen child functions and definitions
+ * The front page template file
  *
+ * If the user has selected a static page for their homepage, this is what will
+ * appear.
+ * Learn more: https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
  */
 
-/**
- * Register widget area.
- *
- */
-function twentyseventeen_child_widgets_init() {
-	register_sidebar( array(
-		'name'          => __( 'Section page partenariats', 'twentyseventeen_child' ),
-		'id'            => 'section-page-partnership',
-		'description'   => __( 'Ajoutez ici des widgets pour la section mise en avant de la page Bons plans & partenariats sur la page d\'accueil.', 'twentyseventeen_child' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
+get_header(); ?>
 
-	register_sidebar( array(
-		'name'          => __( 'Section télécharger application', 'twentyseventeen_child' ),
-		'id'            => 'section-download-app',
-		'description'   => __( 'Ajoutez ici des widgets pour la section mise en avant du téléchargement de l\'application sur la page d\'accueil.', 'twentyseventeen_child' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
-}
-add_action( 'widgets_init', 'twentyseventeen_child_widgets_init' );
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+
+		<?php // Show the selected frontpage content.
+		if ( have_posts() ) :
+			while ( have_posts() ) : the_post();
+				get_template_part( 'template-parts/page/content', 'front-page' );
+			endwhile;
+		else :
+			get_template_part( 'template-parts/post/content', 'none' );
+		endif; ?>
+
+		<?php get_template_part( 'template-parts/page/home/home', 'widgets' ); ?>
+
+		<?php
+		// Get each of our panels and show the post data.
+		if ( 0 !== twentyseventeen_panel_count() || is_customize_preview() ) : // If we have pages to show.
+
+			/**
+			 * Filter number of front page sections in Twenty Seventeen.
+			 *
+			 * @since Twenty Seventeen 1.0
+			 *
+			 * @param int $num_sections Number of front page sections.
+			 */
+			$num_sections = apply_filters( 'twentyseventeen_front_page_sections', 4 );
+			global $twentyseventeencounter;
+
+			// Create a setting and control for each of the sections available in the theme.
+			for ( $i = 1; $i < ( 1 + $num_sections ); $i++ ) {
+				$twentyseventeencounter = $i;
+				twentyseventeen_child_front_page_section( null, $i );
+			}
+
+	endif; // The if ( 0 !== twentyseventeen_panel_count() ) ends here. ?>
+
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_footer();
