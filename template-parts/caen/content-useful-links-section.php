e30c4362776eee331eb74151312eb073e27eebbe
Caroline Hugues <contact@hc-projet1.ovh>
2018-10-17 17:44:15 +0200

    Template updated with new functions

--- template-parts/caen/content-useful-links-section.php
+++ template-parts/caen/content-useful-links-section.php
@@ -4,28 +4,23 @@
  *
  */
 
-$id = 119;
-$included_page = get_post( $id );
-$title = apply_filters('the_title', $included_page->post_title);
-$content = apply_filters('the_content', $included_page->post_content); 
-
 ?>
 
 <article id="useful-links-section">
-	
+    
     <div class="wrap">
-    	<fieldset>
-    		<legend>
-				<header class="entry-header">
-    				<h2 class="entry-title"><?php echo $title; ?></h2>
-    				<?php twentyseventeen_edit_link( get_the_ID() ); ?>
-    			</header><!-- .entry-header -->
-    		</legend>
+        <fieldset>
+            <legend>
+                <header class="entry-header">
+                    <h2 class="entry-title"><?php echo get_title_section(119); ?></h2>
+                    <?php twentyseventeen_edit_link( get_the_ID() ); ?>
+                </header><!-- .entry-header -->
+            </legend>
 
-    		<div class="entry-content-wide">
-        		<?php echo $content; ?>
-    		</div><!-- .entry-content -->
-    	</fieldset>	
-	</div><!-- .wrap -->
-	
+            <div class="entry-content-wide">
+                <?php echo get_content_section(119); ?>
+            </div><!-- .entry-content -->
+        </fieldset> 
+    </div><!-- .wrap -->
+    
 </article><!-- #post-## -->
