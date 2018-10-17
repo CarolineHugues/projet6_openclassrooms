f540e985fc1c85e2655478649dbe7f9049b342a5
Caroline Hugues <contact@hc-projet1.ovh>
2018-10-17 21:44:23 +0200

    Widget area for calendar created

--- functions.php
+++ functions.php
@@ -20,6 +20,16 @@ function twentyseventeen_child_widgets_init() {
 	) );
 
 	register_sidebar( array(
+		'name'          => __( 'Section accueil - télécharger application', 'twentyseventeen_child' ),
+		'id'            => 'section-download-app',
+		'description'   => __( 'Ajoutez ici des widgets pour la section mise en avant du téléchargement de l\'application sur la page d\'accueil.', 'twentyseventeen_child' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+
+	register_sidebar( array(
 		'name'          => __( 'Section télécharger application', 'twentyseventeen_child' ),
 		'id'            => 'section-download-app',
 		'description'   => __( 'Ajoutez ici des widgets pour la section mise en avant du téléchargement de l\'application sur la page d\'accueil.', 'twentyseventeen_child' ),
