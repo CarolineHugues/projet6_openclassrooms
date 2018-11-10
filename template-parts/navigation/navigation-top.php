7b1784676705ca7165e15a917ab6bd8d42f2900d
Caroline Hugues <contact@hc-projet1.ovh>
2018-11-10 17:34:56 +0100

    Search navigation put in a template part

--- template-parts/navigation/navigation-top.php
+++ template-parts/navigation/navigation-top.php
@@ -35,18 +35,7 @@
 			?>
 		<?php endif; ?>
 
-		<button id="openSearch" class="openBtn" onclick="openSearch()"><img src="http://oc-projet6.carolinehugues.ovh/wp-content/uploads/2018/10/icons8-chercher-filled-30.png" /></button>
-
-		<div id="myOverlay" class="overlay">
-		  <span class="closebtn" onclick="closeSearch()" title="Close Overlay">×</span>
-		  <div class="overlay-content">
-		  	<h1>Vous recherchez un évènement, un bon plan ... ?</h1>
-		    <form action="<?php echo esc_url( home_url( '/' ) ); ?>">
-		      <input type="text" placeholder="Rechercher..." value="<?php echo get_search_query(); ?>" name="s">
-		      <button type="submit"><img src="http://oc-projet6.carolinehugues.ovh/wp-content/uploads/2018/10/icons8-chercher-filled-30.png" /></button>
-		    </form>
-		  </div>
-		</div>
+		<?php get_template_part('template-parts/navigation/navigation', 'search'); ?>
 	</section>
 	
 	<?php wp_nav_menu( array(
